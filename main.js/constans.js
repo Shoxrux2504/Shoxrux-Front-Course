6156e9f63267454ccdf3a8125b96cfb10208f447
Mind of Nort <nostalgiabook176@gmail.com>
2025-05-26 15:35:52 +0500

    M

---
+++ main.js/constans.js
@@ -0,0 +1,5 @@
+const MY_NAME = "ORIFJON"
+const MY_PET = "Spike"
+
+
+export{ MY_NAME, MY_PET}
---
+++ main.js/main.js
@@ -0,0 +1,8 @@
+import { MY_PET, MY_NAME } from "./constans.js"
+import { sum } from "./helpers.js"
+console.log(sum(1, 2))
+
+console.log(`
+    my name is: ${MY_NAME}
+    and i have dog called: ${MY_PET}
+    `)
